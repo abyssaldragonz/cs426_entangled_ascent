ea2ad55e39cf7e08d152a94d50d007fd64e7455f
jlewis43-wq <jlewis43@uic.edu>
2026-04-05 19:21:27 -0600

    .

--- Assets/Scripts/BinaryBug.cs
+++ Assets/Scripts/BinaryBug.cs
@@ -3,46 +3,42 @@ using System.Collections;
 using UnityEngine.AI;
 using Unity.VisualScripting;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
-
-// ALL LINES OF CODE WITH "//" IN FRONT OF THEM ARE COMMENTED OUT FOR NOW, 
-// BUT MAY BE RELEVANT TO FUTURE IMPLEMENTATION OF THE SENTRY'S ABILITIES.
-// THEY ARE LEFT IN AS A REFERENCE FOR HOW THE SENTRY'S COMBAT BEHAVIOR MIGHT WORK IN THE FUTURE.
 
 public class BinaryBug: MonoBehaviour
 {
 
     [Header("References")]
-    [SerializeField] private NavMeshAgent navAgent;
     [SerializeField] private Transform playerTransform;
     //[SerializeField] private Transform firePoint;
     //[SerializeField] private GameObject projectilePrefab;
 
     [Header ("Layers")]
-    [SerializeField] private LayerMask terrainLayer;
     [SerializeField] private LayerMask playerLayerMask;
 
     [Header("Patrol Settings")]
-    [SerializeField] private float patrolRadius = 10f;
-    private Vector3 currentPatrolPoint;
-    private bool haspatrolPoint;
 
     [Header("Combat Settings")]
-    private bool isOnAttackCooldown;
     
     [Header("Detection Ranges")]
     [SerializeField] private float visionRange = 20f;
-    [SerializeField] private float engagementRange = 10f;
-    [SerializeField] private float speed = 0.25f;
+    [SerializeField] private float engagementRange = 5f;
+    [SerializeField] private float speed = 5f;
 
     private bool isPlayerVisible;
     private bool isPlayerInRange;
+    private bool detectedPlayer;
+    private bool runInvoke;
 
+    public List<Vector3> playerPositions = new List<Vector3>();
     private Rigidbody rb;
+
+    // ANIMATIONS
+    private Animator anim;
  
     private void Awake()
     {
-
         if (playerTransform == null)
         {
             GameObject playerObj = GameObject.FindWithTag("Player");
@@ -52,11 +48,9 @@ public class BinaryBug: MonoBehaviour
             }
         }
 
-        if (navAgent == null)
-        {
-            navAgent = GetComponent<NavMeshAgent>();
-        }
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }  
+
     private void Update()
     {
         DetectPlayer();
@@ -82,61 +76,74 @@ public class BinaryBug: MonoBehaviour
         isPlayerInRange = Physics.CheckSphere(transform.position, engagementRange, playerLayerMask);
     }
 
-
-
-    private void FindPatrolPoint()
+    // ========== DIGITAL SCENT ==========================================
+    void FollowScentTrail()
     {
-        float randomX = Random.Range(-patrolRadius, patrolRadius);
-        float randomZ = Random.Range(-patrolRadius, patrolRadius);
-        Vector3 potentialPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        // TODO: FIX THIS
+        anim.ResetTrigger("DetectPlayer");
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        playerTransform = playerObj.transform;
+        playerPositions.Add(playerTransform.position); // add the next player position
 
-        // Raycast down from above the potential point to find the ground (using terrainLayer)
-        // if (Physics.Raycast(potentialPoint, transform.up, 2f, terrainLayer))
-        // {
-        //     currentPatrolPoint = potentialPoint;
-        //     haspatrolPoint = true;
-        // }
+        speed = 3f; // slow down
+        float step =  speed * Time.deltaTime; 
+        transform.position = Vector3.MoveTowards(transform.position, playerPositions[0], step);
+        
+        playerPositions.RemoveAt(0); // remove first node 
+        Debug.Log("Bug following scent...");
+        Debug.Log("Player positions:");
+        foreach( var x in playerPositions) {
+            Debug.Log( x.ToString());
+        }
     }
 
 
-    private void PerformPatrol()
-    {
-        if (!haspatrolPoint) FindPatrolPoint();
-
-        if (haspatrolPoint)
-            navAgent.SetDestination(currentPatrolPoint);
-
-        if (Vector3.Distance(transform.position, currentPatrolPoint) < 1f)
-            haspatrolPoint = false;
-    
-    }
-
-
+    // beeline straight towards the player
     private void PerformChase()
     {
+        anim.SetTrigger("DetectPlayer");
+        speed = 5f;
         if (playerTransform != null)
         {
-            // navAgent.SetDestination(playerTransform.position);
             float step =  speed * Time.deltaTime; 
             transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, step);
             GameObject playerObj = GameObject.FindWithTag("Player");
             playerTransform = playerObj.transform;
+            transform.LookAt(playerTransform);
             // Debug.Log(playerTransform.position);
         }
     }
 
-   
-
     private void UpdateBehaviourState()
     {
-        if (!isPlayerVisible && !isPlayerInRange)
+        if (!isPlayerVisible && !isPlayerInRange && detectedPlayer)
         {
-            PerformPatrol();
+            // will now continously go after the player even if out of range
+            if (runInvoke == false)
+            {
+                playerPositions.Add(playerTransform.position);
+                InvokeRepeating(nameof(FollowScentTrail), 0.0f, 3f);
+                runInvoke = true; // just run this once
+            }
         }
 
         else if (isPlayerVisible && !isPlayerInRange)
         {
+            anim.ResetTrigger("AttackMode"); // no longer in attack animation
+            playerPositions.Clear(); // empty out the scent
+            PerformChase(); // and chase
+        }
+
+        else if (isPlayerInRange)
+        {
+            anim.SetTrigger("AttackMode");
             PerformChase();
+        }
+
+        // bug sees player so it marked the scent of player
+        if (isPlayerVisible)
+        {
+            detectedPlayer = true; 
         }
     }
 
@@ -145,8 +152,8 @@ public class BinaryBug: MonoBehaviour
         if (other.gameObject.tag == "Player") {
             Debug.Log("Binary Bug hit the player!");
             other.gameObject.GetComponent<PlayerMovement>().LoseLife();
+
+            anim.SetTrigger("AttackMode");
         }
     }
-}
-
-// ========== Observed Player ========================================
+}   
