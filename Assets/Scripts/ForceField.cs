5714d2a1b15b0a05aa19adf9c5541e6336c6a23c
Josephine <jbl.noetic@gmail.com>
2026-04-18 02:05:00 -0500

    starting on shaders
    added glass and reflective shaders, fixed bug with climbable phasing walls

--- Assets/Scripts/ForceField.cs
+++ Assets/Scripts/ForceField.cs
@@ -15,15 +15,15 @@ public class ForceField : MonoBehaviour
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Player") {
             Debug.Log("Force Field: collision with player detected");
-            var playerRB = other.gameObject.GetComponent<Rigidbody>();
-            // playerRB.linearVelocity = new Vector3(10f, 15f, 10f);
-            // playerRB.linearVelocity -= other.transform.forward * 50f;
-            // playerRB.linearVelocity += other.transform.up * 5f;
-            Vector3 direction = transform.position - other.transform.position;
-            direction.Normalize();
-            direction = Vector3.Reflect(playerRB.linearVelocity, other.contacts[0].normal);
-            // gameObject.GetComponent<Rigidbody>().AddForce(direction * 5000, ForceMode.Impulse);
-            // Debug.Log($"Direction! " + direction);
+            // var playerRB = other.gameObject.GetComponent<Rigidbody>();
+        // playerRB.linearVelocity = new Vector3(10f, 15f, 10f);
+        // playerRB.linearVelocity -= other.transform.forward * 50f;
+        // playerRB.linearVelocity += other.transform.up * 5f;
+            // Vector3 direction = transform.position - other.transform.position;
+            // direction.Normalize();
+            // direction = Vector3.Reflect(playerRB.linearVelocity, other.contacts[0].normal);
+        // gameObject.GetComponent<Rigidbody>().AddForce(direction * 5000, ForceMode.Impulse);
+        // Debug.Log($"Direction! " + direction);
         }
     }
 }
